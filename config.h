9de0e74c8ca8e4abb88411a08a4d650df4ffdec6
Delayless <Delayless@gmail.com>
2021-06-18 11:01:33 +0800

    perf: color theme

--- config.h
+++ config.h
@@ -99,29 +99,28 @@ float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-  /* 8 normal colors */
-  [0] = "#000000", /* black   */
-  [1] = "#ff5555", /* red     */
-  [2] = "#50fa7b", /* green   */
-  [3] = "#f1fa8c", /* yellow  */
-  [4] = "#bd93f9", /* blue    */
-  [5] = "#ff79c6", /* magenta */
-  [6] = "#8be9fd", /* cyan    */
-  [7] = "#bbbbbb", /* white   */
-
-  /* 8 bright colors */
-  [8]  = "#44475a", /* black   */
-  [9]  = "#ff5555", /* red     */
-  [10] = "#50fa7b", /* green   */
-  [11] = "#f1fa8c", /* yellow  */
-  [12] = "#bd93f9", /* blue    */
-  [13] = "#ff79c6", /* magenta */
-  [14] = "#8be9fd", /* cyan    */
-  [15] = "#ffffff", /* white   */
-
-  /* special colors */
-  [256] = "#282a36", /* background */
-  [257] = "#f8f8f2", /* foreground */
+	[0] =  "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	[1] =  "#cc241d",
+	[2] =  "#98971a",
+	[3] =  "#d79921",
+	[4] =  "#458588",
+	[5] =  "#b16286",
+	[6] =  "#689d6a",
+	[7] =  "#a89984",
+	[8]  = "#928374",
+	[9]  = "#fb4934",
+	[10] = "#b8bb26",
+	[11] = "#fabd2f",
+	[12] = "#83a598",
+	[13] = "#d3869b",
+	[14] = "#8ec07c",
+	[15] = "#ebdbb2",
+	[255] = 0,
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#add8e6", /* 256 -> cursor */
+	"#555555", /* 257 -> rev cursor*/
+	"#282828", /* 258 -> bg */
+	"#ebdbb2", /* 259 -> fg */
 };
 
 
@@ -129,9 +128,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor
  */
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
-static unsigned int defaultcs = 257;
+unsigned int defaultfg = 259;
+unsigned int defaultbg = 258;
+static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
 /*
